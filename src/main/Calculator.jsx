5c0e32000da6dff0a19bded5f85eef5d38971baa
davig-sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2019-10-05 07:53:25 -0300

    Added Responsivity

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,10 +4,20 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+};
+
 export default class Calculator extends Component {
     
+    state = { ...initialState };
+
     clearMemory() {
-        console.log('limpar');
+        this.setState({ ...initialState });
     }
     
     setOperation(operation) {
